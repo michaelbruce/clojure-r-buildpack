75042aa436534e2eb2c0f23f8f8a78a0d1e98691
mikepjb <mikepjb@gmail.com>
2018-03-14 12:00:13 +0000

    Set R_HOME

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export R_HOME="/app/vendor/R"
 export R_INCLUDE="/app/vendor/R/lib64/R/include"
 export PATH="/app/vendor/R/bin:/app/vendor/wkhtmltopdf:/app/vendor/.apt/usr/bin:/app/bin:/usr/ruby1.9.2/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export LD_LIBRARY_PATH="/app/vendor/.apt/usr/lib/libblas:/app/vendor/.apt/usr/lib/lapack:/app/vendor/.apt/usr/lib/x86_64-linux-gnu:/app/vendor/.apt/usr/lib/i386-linux-gnu:/app/vendor/.apt/usr/lib:$LD_LIBRARY_PATH"
